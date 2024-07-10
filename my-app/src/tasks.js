280f5909c841e4dfdbf464632e8158d78d906e9e
jessfraser8 <jfraser4@usf.edu>
2024-07-10 12:30:57 -0400

    Set up tasks module.

---
+++ my-app/src/tasks.js
@@ -0,0 +1,20 @@
+// tasks.js
+// Define tasks.
+
+tasks = [
+    {
+        id: 1,
+        name: "Complete the React Challenge",
+        isCompleted: false,
+    },
+    {
+        id: 2,
+        name: "Get Grocerices",
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        name: "Clean the House",
+        isCompleted: false,
+    }
+]
